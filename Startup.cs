b6298044ad6840e13ac11c9757f7e4e2f9ede31b
Robert Kuhfß <robert.kuhfss@xitaso.com>
2019-08-14 16:24:08 +0200

    Added swagger

--- Startup.cs
+++ Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace AspNetCoreHerokuDocker
 {
@@ -19,6 +20,11 @@ namespace AspNetCoreHerokuDocker
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,6 +42,12 @@ namespace AspNetCoreHerokuDocker
 
             app.UseHttpsRedirection();
             app.UseMvc();
+            app.UseSwagger();
+            
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
         }
     }
 }
